8d7af56be7bcab6b2de28629487892d762a1d2f9
lixiaojing <lixiaojing@etaostars.com>
2015-11-09 15:12:11 +0800

    提交文件

--- Easy.Rpc/Properties/AssemblyInfo.cs
+++ Easy.Rpc/Properties/AssemblyInfo.cs
@@ -1,31 +1,28 @@
-﻿#region Using directives
-
+#region Using directives
 using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
 #endregion
-
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Easy.Rpc")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Easy.Rpc")]
-[assembly: AssemblyCopyright("Copyright 2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
+[assembly: AssemblyTitle ("Easy.Rpc")]
+[assembly: AssemblyDescription ("RPC调用类库，主要提供负载均衡、容错调用算法")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("李晓静")]
+[assembly: AssemblyProduct ("Easy.Rpc")]
+[assembly: AssemblyCopyright ("Copyright 2015")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
 // This sets the default COM visibility of types in the assembly to invisible.
 // If you need to expose a type to COM, use [ComVisible(true)] on that type.
-[assembly: ComVisible(false)]
-
+[assembly: ComVisible (false)]
 // The assembly version has following format :
 //
 // Major.Minor.Build.Revision
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion ("1.0.0.2")]
+[assembly: AssemblyFileVersion ("1.0.0.2")]
