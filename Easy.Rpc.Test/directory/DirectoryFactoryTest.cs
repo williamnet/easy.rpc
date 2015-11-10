cd2add21d4d6dc62496a4953d7410b0b674cb813
lixiaojing <lixiaojing@etaostars.com>
2015-11-10 18:26:20 +0800

    提交文件

---
+++ Easy.Rpc.Test/directory/DirectoryFactoryTest.cs
@@ -0,0 +1,26 @@
+﻿using Easy.Rpc.directory;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy.Rpc.Test.directory
+{
+    [TestClass]
+    public class DirectoryFactoryTest
+    {
+        [TestMethod]
+        public void RegisterTest()
+        {
+            //注册提供者目录
+            //在实际代码中使用时，此方法调用应该放在应用启动时，如果时Web应用程序 ，需要放在Application_Start中调用
+            DirectoryFactory.Register("order", new StaticDirectory(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "rpc.config"), "order"));
+
+            IDirectory directory = DirectoryFactory.GetDirectory("order");
+            Assert.IsNotNull(directory);
+        }
+    }
+}
---
+++ Easy.Rpc.Test/directory/RedisDirectoryTest.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Easy.Rpc.LoadBalance;
+using Easy.Rpc.directory;
+using Newtonsoft.Json;
+using StackExchange.Redis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RandomTest
+{
+	[TestClass]
+	public class RedisDirectoryTest
+	{
+		[TestMethod]
+		public void TestMethod()
+		{
+			var newRedis = new RedisServer("172.18.11.83:6379", 3);
+			var directory = new RedisDirectory(newRedis, "OrderService");
+			
+			var nodes = directory.GetNodes();
+
+            Assert.IsTrue(nodes.Count > 0);
+		}
+	}
+	
+}
