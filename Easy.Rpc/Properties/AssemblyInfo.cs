b4d3bdeba6f8b5ba6a7c3d748e98dce0bffca7d3
lixiaojing <lixiaojing@etaostars.com>
2016-04-19 14:13:49 +0800

    d

--- Easy.Rpc/Properties/AssemblyInfo.cs
+++ Easy.Rpc/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("李晓静")]
 [assembly: AssemblyProduct ("Easy.Rpc")]
-[assembly: AssemblyCopyright ("Copyright 2015")]
+[assembly: AssemblyCopyright("Copyright 2016")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 // This sets the default COM visibility of types in the assembly to invisible.
@@ -24,5 +24,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.1.2")]
-[assembly: AssemblyFileVersion("1.0.1.2")]
+[assembly: AssemblyVersion("1.0.1.3")]
+[assembly: AssemblyFileVersion("1.0.1.3")]
